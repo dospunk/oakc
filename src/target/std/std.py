3a4cfaa3553ff99c6091f2e47c7617ea6483a754
dospunk <dospunk@users.noreply.github.com>
2020-08-19 15:18:22 -0400

    std.py completed, unfinished. Need to find a way to deal with pylint's "undefined-variable" errors

---
+++ src/target/std/std.py
@@ -0,0 +1,33 @@
+import getch as getchar
+
+def prn(vm: Machine) -> None:
+	'''print a number'''
+	n = machine_pop(vm)
+	print(n)
+
+def prs(vm: Machine) -> None:
+	'''print a null-terminated string'''
+	addr = machine_pop(vm)
+	i = addr
+	while vm.memory[i]:
+		print(chr(vm.memory[i]), end='')
+		i += 1
+
+def prc(vm: Machine) -> None:
+	'''print a char'''
+	n = machine_pop(vm)
+	print(chr(n))
+
+
+def prend(vm: Machine) -> None:
+	'''print a newline'''
+	#print inserts a newline
+	print("")
+
+def getch(vm: Machine) -> None:
+	ch = getchar.getch()
+	if ch == '\r':
+		ch = getchar.getch()
+	machine_push(vm, ch)
+
+
